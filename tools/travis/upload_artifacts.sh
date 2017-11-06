20b87a650d7226ff64cca4992ab904b55acde639
Andrew O'Rourke <andrew@montagne.uk>
2017-11-06 20:11:01 +0000

    Take two.

--- tools/travis/upload_artifacts.sh
+++ tools/travis/upload_artifacts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ "$BUILD_TESTING" = true ] && ["$TRAVIS_PULL_REQUEST" = false]; then
+if [ "$BUILD_TESTING" = false ] && [ "$TRAVIS_PULL_REQUEST" = false ]; then
 	aws s3 cp tgstation.dmb "s3://s3.oraclestation.com/$TRAVIS_BRANCH/$TRAVIS_JOB_NUMBER/tgstation.dmb"
 	aws s3 cp tgstation.rsc "s3://s3.oraclestation.com/$TRAVIS_BRANCH/$TRAVIS_JOB_NUMBER/tgstation.rsc"
 	aws s3 cp tgstation.dmb "s3://s3.oraclestation.com/$TRAVIS_BRANCH/latest/tgstation.dmb"
