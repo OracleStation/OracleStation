db61b8b906173832183fe1d0ece4df412d59056d
Andrew O'Rourke <andrew@montagne.uk>
2017-11-06 20:34:27 +0000

    It helps if you close the if statement, Andy.

--- tools/travis/upload_artifacts.sh
+++ tools/travis/upload_artifacts.sh
@@ -5,3 +5,4 @@ if [ "$BUILD_TESTING" = true ] && [ "$TRAVIS_PULL_REQUEST" = false ]; then
 	aws s3 cp tgstation.rsc "s3://s3.oraclestation.com/$TRAVIS_BRANCH/$TRAVIS_JOB_NUMBER/tgstation.rsc"
 	aws s3 cp tgstation.dmb "s3://s3.oraclestation.com/$TRAVIS_BRANCH/latest/tgstation.dmb"
 	aws s3 cp tgstation.rsc "s3://s3.oraclestation.com/$TRAVIS_BRANCH/latest/tgstation.rsc"
+fi
