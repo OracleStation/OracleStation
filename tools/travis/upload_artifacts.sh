74c5179926b14a9317963ba9b03991983d421622
Andrew O'Rourke <andrew@montagne.uk>
2017-11-29 01:03:26 +0000

    Changed to zip up artifacts.

--- tools/travis/upload_artifacts.sh
+++ tools/travis/upload_artifacts.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 if [ "$BUILD_TESTING" = true ] && [ "$TRAVIS_PULL_REQUEST" = false ]; then
-	aws s3 cp tgstation.dmb "s3://s3.oraclestation.com/$TRAVIS_BRANCH/$TRAVIS_JOB_NUMBER/tgstation.dmb"
-	aws s3 cp tgstation.rsc "s3://s3.oraclestation.com/$TRAVIS_BRANCH/$TRAVIS_JOB_NUMBER/tgstation.rsc"
-	aws s3 cp tgstation.dmb "s3://s3.oraclestation.com/$TRAVIS_BRANCH/latest/tgstation.dmb"
-	aws s3 cp tgstation.rsc "s3://s3.oraclestation.com/$TRAVIS_BRANCH/latest/tgstation.rsc"
+	zip -r tgstation.zip _maps tgstation.dmb tgstation.rsc
+	zip -r resources.zip tgstation.rsc
+	aws s3 cp tgstation.dmb "s3://s3.oraclestation.com/$TRAVIS_BRANCH/$TRAVIS_JOB_NUMBER/tgstation.zip"
+	aws s3 cp tgstation.rsc "s3://s3.oraclestation.com/$TRAVIS_BRANCH/$TRAVIS_JOB_NUMBER/resources.zip"
+	aws s3 cp tgstation.dmb "s3://s3.oraclestation.com/$TRAVIS_BRANCH/latest/tgstation.zip"
+	aws s3 cp tgstation.rsc "s3://s3.oraclestation.com/$TRAVIS_BRANCH/latest/resources.zip"
 fi
