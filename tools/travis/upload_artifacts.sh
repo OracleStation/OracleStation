a714d6ef4943179ea08945f12cdca6980880b866
Andrew O'Rourke <andrew@montagne.uk>
2017-11-29 01:21:56 +0000

    Andrew is an idiot.

--- tools/travis/upload_artifacts.sh
+++ tools/travis/upload_artifacts.sh
@@ -3,8 +3,8 @@
 if [ "$BUILD_TESTING" = true ] && [ "$TRAVIS_PULL_REQUEST" = false ]; then
 	zip -r tgstation _maps tgstation.dmb tgstation.rsc
 	zip -r resources tgstation.rsc
-	aws s3 cp tgstation.dmb "s3://s3.oraclestation.com/$TRAVIS_BRANCH/$TRAVIS_JOB_NUMBER/tgstation.zip"
-	aws s3 cp tgstation.rsc "s3://s3.oraclestation.com/$TRAVIS_BRANCH/$TRAVIS_JOB_NUMBER/resources.zip"
-	aws s3 cp tgstation.dmb "s3://s3.oraclestation.com/$TRAVIS_BRANCH/latest/tgstation.zip"
-	aws s3 cp tgstation.rsc "s3://s3.oraclestation.com/$TRAVIS_BRANCH/latest/resources.zip"
+	aws s3 cp tgstation.zip "s3://s3.oraclestation.com/$TRAVIS_BRANCH/$TRAVIS_JOB_NUMBER/tgstation.zip"
+	aws s3 cp resources.zip "s3://s3.oraclestation.com/$TRAVIS_BRANCH/$TRAVIS_JOB_NUMBER/resources.zip"
+	aws s3 cp tgstation.zip "s3://s3.oraclestation.com/$TRAVIS_BRANCH/latest/tgstation.zip"
+	aws s3 cp resources.zip "s3://s3.oraclestation.com/$TRAVIS_BRANCH/latest/resources.zip"
 fi
