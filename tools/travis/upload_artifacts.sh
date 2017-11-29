a8c77e269041fa2d5002b7b723996573b80039ff
Andrew O'Rourke <andrew@montagne.uk>
2017-11-29 02:10:01 +0000

    Store commit hash.

--- tools/travis/upload_artifacts.sh
+++ tools/travis/upload_artifacts.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 if [ "$BUILD_TESTING" = true ] && [ "$TRAVIS_PULL_REQUEST" = false ]; then
-	zip -r tgstation _maps tgstation.dmb tgstation.rsc
+	echo "$TRAVIS_COMMIT" > COMMIT_HASH
+	zip -r tgstation _maps tgstation.dmb tgstation.rsc COMMIT_HASH
 	zip -r resources tgstation.rsc
 	aws s3 cp tgstation.zip "s3://s3.oraclestation.com/$TRAVIS_BRANCH/$TRAVIS_JOB_NUMBER/tgstation.zip"
 	aws s3 cp resources.zip "s3://s3.oraclestation.com/$TRAVIS_BRANCH/$TRAVIS_JOB_NUMBER/resources.zip"
+	aws s3 cp COMMIT_HASH   "s3://s3.oraclestation.com/$TRAVIS_BRANCH/$TRAVIS_JOB_NUMBER/COMMIT_HASH"
 	aws s3 cp tgstation.zip "s3://s3.oraclestation.com/$TRAVIS_BRANCH/latest/tgstation.zip"
 	aws s3 cp resources.zip "s3://s3.oraclestation.com/$TRAVIS_BRANCH/latest/resources.zip"
+	aws s3 cp COMMIT_HASH   "s3://s3.oraclestation.com/$TRAVIS_BRANCH/latest/COMMIT_HASH"
 fi
