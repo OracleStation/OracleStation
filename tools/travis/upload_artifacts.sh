be5f06a062fadc49cdbf77dd18971fabda24a2e7
Andrew O'Rourke <andrew@montagne.uk>
2017-11-29 01:16:02 +0000

    Fix.

--- tools/travis/upload_artifacts.sh
+++ tools/travis/upload_artifacts.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ "$BUILD_TESTING" = true ] && [ "$TRAVIS_PULL_REQUEST" = false ]; then
-	zip -r tgstation.zip _maps tgstation.dmb tgstation.rsc
-	zip -r resources.zip tgstation.rsc
+	zip -r tgstation _maps tgstation.dmb tgstation.rsc
+	zip -r resources tgstation.rsc
 	aws s3 cp tgstation.dmb "s3://s3.oraclestation.com/$TRAVIS_BRANCH/$TRAVIS_JOB_NUMBER/tgstation.zip"
 	aws s3 cp tgstation.rsc "s3://s3.oraclestation.com/$TRAVIS_BRANCH/$TRAVIS_JOB_NUMBER/resources.zip"
 	aws s3 cp tgstation.dmb "s3://s3.oraclestation.com/$TRAVIS_BRANCH/latest/tgstation.zip"
