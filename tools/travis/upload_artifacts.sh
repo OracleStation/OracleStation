cce8674f54ddc9780a7030c6231d5119c81cf498
Andrew O'Rourke <andrew@montagne.uk>
2017-12-08 01:15:38 +0000

    CI config.

--- tools/travis/upload_artifacts.sh
+++ tools/travis/upload_artifacts.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 if [ "$BUILD_TESTING" = true ] && [ "$TRAVIS_PULL_REQUEST" = false ] && [ $AWS_SECRET_ACCESS_KEY ]; then
+	echo "Building changelog..."
+	python3 tools/ss13_genchangelog.py html/changelogs/.all_changelog.yml $GITHUB_ACCOUNT_NAME $GITHUB_ACCESS_TOKEN
+	python3 tools/ss13_genchangelog.py html/changelog.html html/changelogs
+
 	echo "Creating archives..."
 	echo "$TRAVIS_COMMIT" > COMMIT_HASH
-	zip -r tgstation _maps tgstation.dmb tgstation.rsc COMMIT_HASH > /dev/null
+	zip -r tgstation _maps strings sound html tgstation.dmb tgstation.rsc COMMIT_HASH > /dev/null
 	echo "tgstation.zip completed"
 	zip -r resources tgstation.rsc > /dev/null
 	echo "resources.zip completed"
@@ -13,10 +17,7 @@ if [ "$BUILD_TESTING" = true ] && [ "$TRAVIS_PULL_REQUEST" = false ] && [ $AWS_S
 	zip -T resources.zip
 
 	echo "Uploading to S3..."
-	# aws s3 cp tgstation.zip "s3://s3.us-east-1.oraclestation.com/$TRAVIS_BRANCH/$TRAVIS_JOB_NUMBER/tgstation.zip"
-	# aws s3 cp resources.zip "s3://s3.us-east-1.oraclestation.com/$TRAVIS_BRANCH/$TRAVIS_JOB_NUMBER/resources.zip"
-	# aws s3 cp COMMIT_HASH   "s3://s3.us-east-1.oraclestation.com/$TRAVIS_BRANCH/$TRAVIS_JOB_NUMBER/COMMIT_HASH"
 	aws s3 cp tgstation.zip "s3://s3.us-east-1.oraclestation.com/$TRAVIS_BRANCH/latest/tgstation.zip"
-	aws s3 cp resources.zip "s3://s3.us-east-1.oraclestation.com/$TRAVIS_BRANCH/latest/resources.zip"
+	aws s3 cp resources.zip "s3://s3.us-east-1.oraclestation.com/resources/$TRAVIS_COMMIT.rsc.zip"
 	aws s3 cp COMMIT_HASH   "s3://s3.us-east-1.oraclestation.com/$TRAVIS_BRANCH/latest/COMMIT_HASH"
 fi
