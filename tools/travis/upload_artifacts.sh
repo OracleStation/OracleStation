d146f0b3fa7ec3662a0a5726b0f78312ddd5ef36
Andrew O'Rourke <andrew@montagne.uk>
2017-12-08 01:46:25 +0000

    It helps if you use the correct script

--- tools/travis/upload_artifacts.sh
+++ tools/travis/upload_artifacts.sh
@@ -2,7 +2,7 @@
 
 if [ "$BUILD_TESTING" = true ] && [ "$TRAVIS_PULL_REQUEST" = false ] && [ $AWS_SECRET_ACCESS_KEY ]; then
 	echo "Building changelog..."
-	python3 tools/ss13_genchangelog.py html/changelogs/.all_changelog.yml $GITHUB_ACCOUNT_NAME $GITHUB_ACCESS_TOKEN
+	python3 tools/pull_changelogs.py html/changelogs/.all_changelog.yml $GITHUB_ACCOUNT_NAME $GITHUB_ACCESS_TOKEN
 	python3 tools/ss13_genchangelog.py html/changelog.html html/changelogs
 
 	echo "Creating archives..."
