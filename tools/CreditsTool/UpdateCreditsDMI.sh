2b26b1a3aca9d8c96d0ce867833219ef1afaa71d
AffectedArc07 <josephcase55@hotmail.com>
2018-02-18 12:24:31 +0000

    Makes the credits show our contributors instead of just TG (#641)
    * Makes the credits ours
    
    * I think this is what Purpose wanted

--- tools/CreditsTool/UpdateCreditsDMI.sh
+++ tools/CreditsTool/UpdateCreditsDMI.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #If you hit github's rate limit, add a 3rd parameter here that is a github personal access token
-./CreditsTool tgstation tgstation
+./CreditsTool OracleStation OracleStation
 
 rm ../../icons/credits.dmi
 
